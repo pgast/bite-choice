ff1ce2ce36568e9979d07bbf363e891be2eedd68
Pablo Gastelum Leyva <pablogl@Pablos-MacBook-Air.local>
2023-03-03 16:20:30 -0700

    changes

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,17 @@
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
-  // config.resolve.fallback = {
-  //   "fs": false,
-  //   "tls": false,
-  //   "net": false,
-  //   "path": false,
-  //   "zlib": false,
-  //   "http": false,
-  //   "https": false,
-  //   "stream": false,
-  //   "crypto": false,
-  // }
-
-  // config.resolve.alias = {
-  //   process: "process/browser"
-  // }
+  config.resolve.fallback = {
+    "fs": false,
+    "tls": false,
+    "net": false,
+    "path": false,
+    "zlib": false,
+    "http": require.resolve("stream-http"),
+    "https": false,
+    "stream": false,
+    "crypto": false,
+    "querystring": false,
+  }
 
   return config;
 }
