29e06754f1a2b987a5e95f4c303ae3888f0a1118
Pablo Gastelum Leyva <pablogl@Pablos-MacBook-Air.local>
2023-03-04 19:42:38 -0700

    changes

--- config-overrides.js
+++ config-overrides.js
@@ -8,18 +8,19 @@ module.exports = override(
     })
   ),
   addWebpackResolve({
-    "fs": false,
-    "tls": false,
-    "net": false,
-    "path": false,
-    "zlib": false,
-    "http": require.resolve("stream-http"),
-    "url": require.resolve("url"),
-    "https": false,
-    "stream": false,
-    "crypto": false,
-    "util": false,
-    "querystring": false,
+    fallback: {
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": require.resolve("stream-http"),
+      "url": require.resolve("url"),
+      "https": false,
+      "stream": false,
+      "crypto": false,
+      "util": false,
+    }
   })
 )
 
