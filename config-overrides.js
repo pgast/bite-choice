4ea5a6e493cc99dd772681c6bc31e4cf839f5b7f
Pablo Gastelum Leyva <pablogl@Pablos-MacBook-Air.local>
2023-03-04 19:37:01 -0700

    changes

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,30 @@
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  config.resolve.fallback = {
-    "fs": false,
-    "tls": false,
-    "net": false,
-    "path": false,
-    "zlib": false,
-    "http": require.resolve("stream-http"),
-    "url": require.resolve("url"),
-    "https": false,
-    "stream": false,
-    "crypto": false,
-    "querystring": false,
-  }
+const webpack = require('webpack')
+const { override, addWebpackPlugin } = require('customize-cra')
 
-  return config;
-}
+module.exports = override(
+  addWebpackPlugin(
+    new webpack.DefinePlugin({
+      process: { env: {} },
+    })
+  )
+)
+
+// module.exports = function override(config, env) {
+//   //do stuff with the webpack config...
+//   config.resolve.fallback = {
+//     "fs": false,
+//     "tls": false,
+//     "net": false,
+//     "path": false,
+//     "zlib": false,
+//     "http": require.resolve("stream-http"),
+//     "url": require.resolve("url"),
+//     "https": false,
+//     "stream": false,
+//     "crypto": false,
+//     "util": false,
+//     "querystring": false,
+//   }
+
+//   return config;
+// }
