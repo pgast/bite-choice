71641e7151c50e822caad275bddc6977cf8407a3
Pablo Gastelum Leyva <pablogl@Pablos-MacBook-Air.local>
2023-03-05 18:49:02 -0700

    fix

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,7 @@ const { override, addWebpackPlugin, addWebpackResolve } = require('customize-cra
 module.exports = override(
   addWebpackPlugin(
     new webpack.DefinePlugin({
-      process: "process/browser",
+      'process.env.YELP_API_KEY': JSON.stringify(process.env.YELP_API_KEY),
     })
   ),
   addWebpackResolve({
