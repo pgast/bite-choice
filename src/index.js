0eaacfd925dc872f2ab0d037796ac27bb27b6f1a
Pablo Gastelum Leyva <pablogl@Pablos-MacBook-Air.local>
2023-03-05 18:53:57 -0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-import * as serviceWorker from './serviceWorker';
 import { StoreProvider } from './Store';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(<StoreProvider><App /></StoreProvider>, document.getElementById('root'));
-
-serviceWorker.unregister();
