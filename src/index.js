5af7661ce25c2756a32148e454d8e5239fad3747
Pablo Gastelum Leyva <pablogl@Pablos-MacBook-Air.local>
2023-03-08 19:35:22 -0700

    fix

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import React from 'react';
 import { StoreProvider } from './Store';
 import App from './App';
 import './index.css';
+window.Buffer = window.Buffer || require("buffer").Buffer;
 
 ReactDOM.render(<StoreProvider><App /></StoreProvider>, document.getElementById('root'));
